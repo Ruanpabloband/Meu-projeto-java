8302288dd5e9f25c784de0cbb09491340a725a94
Ruan Pablo Bandeira de Oliveira <166414190+Ruanpabloband@users.noreply.github.com>
2025-08-01 18:44:51 -0300

    Primeiros projetos

---
+++ two_project.java
@@ -0,0 +1,13 @@
+import java.util.Locale;
+
+public class two_project {
+public static void main (String[] args){
+    Locale.setDefault(Locale.US);
+
+    System.out.println("Soma de 2 numeros:");
+
+    int n1 = 3, n2 = 2, n3 = n1 + n2;
+
+    System.out.println(n3);
+}
+}
