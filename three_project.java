5675468f9faa530c6bed88fd2e95ecd3d59ec04b
Ruan Pablo Bandeira de Oliveira <166414190+Ruanpabloband@users.noreply.github.com>
2025-08-01 19:34:37 -0300

    Primeiros projetos

--- three_project.java
+++ three_project.java
@@ -3,6 +3,10 @@ import java.util.Locale;
 public class three_project {
     public static void main (String[] args){
         Locale.setDefault(Locale.US);
+int n1 = 10;
+int n2 = 2;
+int n3 = n1 - n2;
 
+        System.out.println(n3);
     }
 }
